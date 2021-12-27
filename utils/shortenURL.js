1ff55f52405c1746de4ee97b8836a5a5bad73b19
elliecw <hanchig@gmail.com>
2021-12-27 18:44:29 +0800

    no message

--- utils/shortenURL.js
+++ utils/shortenURL.js
@@ -1,5 +1,5 @@
 /** 字元表，由 0-9、a-z、A-Z 組成 */
-const BASE_62_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const BASE_62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 /** 字元表的最大 Index */
 const MAX = 61
 /** 字元表的最小 Index */
@@ -17,7 +17,7 @@ module.exports = (shortenURL_Length) => {
     /** 產生亂數 Index */
     const randomIndex = Math.floor(Math.random() * (MAX - MIN + 1) + MIN)
     /** 依照亂數表找出對應的字元 */
-    const chooseChar = BASE_62_CHAR[randomIndex]
+    const chooseChar = BASE_62[randomIndex]
     /** 將對應字元放入 result */
     result += chooseChar
   }
