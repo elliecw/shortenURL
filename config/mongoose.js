2699f4dc348eb9a147a93e64474fdc0bd3f794e0
elliecw <hanchig@gmail.com>
2021-12-27 18:43:20 +0800

    Revise mongoose connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost/url-shortener", {
+mongoose.connect("mongodb://localhost/shortenURL", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
